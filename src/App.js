e5814360dbafc53bea5253d7bc06e98a8e5e2c3a
Edlee Ducay <55123805+EdleeDucay@users.noreply.github.com>
2021-05-21 15:46:09 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react'
+import {
+  Route, BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
+import LogInPage from './LogInPage';
+import HomePage from './HomePage';
+
+function App() {
+  return (
+    <Router>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+      <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={LogInPage}/>
+          <Route exact path="/home" component={HomePage}/>
+        </Switch>
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ src/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.css'
+import {Link} from 'react-router-dom'
+
+export default class HomePage extends React.Component{
+    constructor(props){
+        super(props)
+
+    }
+    render(){
+        return(
+            <div id="container-wrapper">
+                HOME
+            </div>
+        )
+    }
+}
