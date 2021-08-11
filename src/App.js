765160faefca58f599455ec287e421dea3dfe3ad
Edlee Ducay <55123805+EdleeDucay@users.noreply.github.com>
2021-08-11 01:25:33 -0600

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import React from 'react'
 import {AuthProvider} from './contexts/AuthContext'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react'
-import {Button, Alert} from 'react-bootstrap'
-import "../styles/Form.css"
+import {Button, Alert, Navbar, Container, Nav, Form, FormControl} from 'react-bootstrap'
+import "../styles/Home.css"
 import {useAuth} from '../contexts/AuthContext'
 import { Link, useHistory } from 'react-router-dom'
 
@@ -22,10 +22,47 @@ export default function HomePage() {
 
     return (
         <>
-        <div>
-        HELLO
         {error && <Alert variant='danger'>{error}</Alert>}
-        <button onClick={handleLogout}>Logout</button>
+        <Navbar expand="lg" bg="dark" variant="dark">
+
+            {/* <Container> */}
+            <Navbar.Brand className="px-3">
+                <Link to="/" className="homeBrand">
+                <img src='img/Youtube.png' alt='YouTube-Logo' className='youtubeLogoHome'/>
+                        Youtube Custom Playlists
+                </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link className=""> <Link to="/" className="homeLink px-3">My Playlists</Link></Nav.Link>
+                <Form className="d-flex px-5">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="mr-3"
+                        aria-label="Search"
+                        style={{minWidth: '500px', 
+                            backgroundColor: 'rgb(50, 50, 50)',
+                            color: 'white',
+                            borderColor: 'rgb(50, 50, 50)'}}
+                    />
+                    <Button variant="secondary">Search</Button>
+                </Form>
+            </Nav>
+            
+            <Nav >
+                <Nav.Link className="px-3 homeEmail" disabled>{currentUser.email}</Nav.Link>
+                <Button variant="secondary" className="mx-3" onClick={handleLogout}>Logout</Button>
+
+            </Nav>
+            </Navbar.Collapse>
+            {/* </Container> */}
+
+        
+        </Navbar>
+        <div className="homeBody">
+            HELLO
         </div>
         </>
        
